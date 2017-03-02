73369596f6723b5f23b664df718de0dd4a93b3f2
yoPitarch <pitarch@irit.fr>
2017-03-02 17:40:05 +0100

    Add features.txt

---
+++ collectRawData/run_get_raw_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --job-name=getRawData
+#SBATCH --output=/projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/collectRawData/output.txt
+#SBATCH --mail-user=pitarch@irit.fr
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks=1
+
+/logiciels/Python-3.5.2/bin/python3 /projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/collectRawData/get_raw_data.py
