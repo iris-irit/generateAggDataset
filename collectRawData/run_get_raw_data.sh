0416c392391ce3ac595dacb932a6a56682424151
yoPitarch <pitarch@irit.fr>
2017-03-02 17:40:46 +0100

    Add features.txt

--- collectRawData/run_get_raw_data.sh
+++ collectRawData/run_get_raw_data.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
 
-/logiciels/Python-3.5.2/bin/python3 /projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/collectRawData/get_raw_data.py
+srun /logiciels/Python-3.5.2/bin/python3 /projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/collectRawData/get_raw_data.py
