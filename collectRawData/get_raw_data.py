f83ad1ab6eb026848bf11ae769b754bff4308e3c
yoPitarch <pitarch@irit.fr>
2017-03-03 07:19:39 +0100

    Add features.txt

--- collectRawData/get_raw_data.py
+++ collectRawData/get_raw_data.py
@@ -25,7 +25,7 @@ def extractDescription(soup):
 		else :
 			og_title = soup.find("meta",  property="og:title")
 			print("og:title",og_title)
-			if og_title :
+			if og_title and "content" in og_title:
 				return og_title["content"]
 			else :
 				return soup.find("title").text
@@ -47,6 +47,10 @@ def getType(soup) :
 			return "website"
 	else :
 		return "website"
+
+
+def alreadyExtracted(eventId) :
+	return os.path.exists("/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/mapping/mapping.json")
 
 
 
@@ -72,6 +76,7 @@ if DEBUG :
 	print(extractDescription(s))
 
 	sys.exit()
+
 
 
 
@@ -113,212 +118,215 @@ sys.exit()
 
 
 for eventId in listEventOk :
+
+	if not alreadyExtracted(eventId) :
+
 #for eventId in [3] :
-	mapping = {}
-	id = 0
-	inverse_mapping = {}
+		mapping = {}
+		id = 0
+		inverse_mapping = {}
 
-	# Création du répertoire pour chaque évènement
-	dir_data = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)
-	dir_urls = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/urls/"
-	dir_img = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/images/"
-	dir_videos = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/videos/"
-	dir_index = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/to_index/"
-	dir_mapping = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/mapping/"
-	os.system("rm -r "+dir_data)
-	os.system("mkdir "+dir_data)
-	os.system("mkdir " + dir_data+"/urls")
-	os.system("mkdir " + dir_data+"/images")
-	os.system("mkdir " + dir_data+"/videos")
-	os.system("mkdir " + dir_data+"/to_index")
-	os.system("mkdir " + dir_data+"/mapping")
-
-
-	print("*********",eventId,"*********")
-	# Lecture des fichiers json
-	path_json = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Preliminaries/data_"+str(eventId)+".txt"
-	f = open(path_json,"r")
-	js = json.load(f)
-	f.close()
-
-	# Parcours des tweets pour chaque évènement
-	for tw in js :
-		if "entities" in tw :
-
-			id_tweet = str(tw["id"])
-
-			text_tweet = tw["text"]
-			text_tweet = re.sub(r"http\S+",'',text_tweet)
-
-			# Si le texte n'est pas vide, on crée un enregistrement associé au tweet
-			if id_tweet not in inverse_mapping :
-				id = len(inverse_mapping)
-				inverse_mapping[id_tweet] = id
-				mapping[id] = {"origin":str(id_tweet), "type":"tweet", "tweets":[id_tweet],"description":text_tweet}
+		# Création du répertoire pour chaque évènement
+		dir_data = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)
+		dir_urls = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/urls/"
+		dir_img = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/images/"
+		dir_videos = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/videos/"
+		dir_index = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/to_index/"
+		dir_mapping = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/" + str(eventId)+"/mapping/"
+		os.system("rm -r "+dir_data)
+		os.system("mkdir "+dir_data)
+		os.system("mkdir " + dir_data+"/urls")
+		os.system("mkdir " + dir_data+"/images")
+		os.system("mkdir " + dir_data+"/videos")
+		os.system("mkdir " + dir_data+"/to_index")
+		os.system("mkdir " + dir_data+"/mapping")
 
 
-			# On s'occupe du champ media (les images uploadees)
-			if "media" in tw["entities"] :
-				for m in tw["entities"]["media"] :
-					try:
-						id_media = id_tweet+"_upload_"+m["media_url"].split("/")[-1]
-						origin = m["media_url"]
-						urllib.request.urlretrieve(origin, dir_img+id_media)
+		print("*********",eventId,"*********")
+		# Lecture des fichiers json
+		path_json = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Preliminaries/data_"+str(eventId)+".txt"
+		f = open(path_json,"r")
+		js = json.load(f)
+		f.close()
 
-						# Ajout dans la structure de mapping de l'image uploadee
-						if id_media not in inverse_mapping:
-							id = len(inverse_mapping)
-							inverse_mapping[id_media] = id
-							mapping[id] = {"origin": origin, "type": "img_upload", "tweets": [id_tweet], "description":text_tweet}
+		# Parcours des tweets pour chaque évènement
+		for tw in js :
+			if "entities" in tw :
 
+				id_tweet = str(tw["id"])
 
-					except urllib.error.HTTPError:
-						print("404")
+				text_tweet = tw["text"]
+				text_tweet = re.sub(r"http\S+",'',text_tweet)
 
-
-			# On s'occupe des entités (urls + images)
-			if "urls" in tw["entities"] :
-				for u in tw["entities"]["urls"] :
-					url = u["expanded_url"]
-					#print("************************************")
-					print("\t",url)
-					try :
-						req = requests.get(url,verify=False, timeout=5)
-
-						if req.status_code == 200 :
-							id_media = req.url
-							origin = req.url
-
-							# Gestion image (le lien est directement une image) :
-							if isImg(req.url) :
-								try:
-									id_media = req.url
-									origin = req.url
-									urllib.request.urlretrieve(req.url, dir_img + "remote_" + req.url.split("/")[-1])
-
-									# Ajout dans la structure de mapping de l'image url
-									if id_media not in inverse_mapping:
-										id = len(inverse_mapping)
-										inverse_mapping[id_media] = id
-										mapping[id] = {"origin": origin, "type": "img_url", "tweets": [id_tweet], "description": text_tweet}
-									else :
-										mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
-										mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+				# Si le texte n'est pas vide, on crée un enregistrement associé au tweet
+				if id_tweet not in inverse_mapping :
+					id = len(inverse_mapping)
+					inverse_mapping[id_tweet] = id
+					mapping[id] = {"origin":str(id_tweet), "type":"tweet", "tweets":[id_tweet],"description":text_tweet}
 
 
+				# On s'occupe du champ media (les images uploadees)
+				if "media" in tw["entities"] :
+					for m in tw["entities"]["media"] :
+						try:
+							id_media = id_tweet+"_upload_"+m["media_url"].split("/")[-1]
+							origin = m["media_url"]
+							urllib.request.urlretrieve(origin, dir_img+id_media)
 
-								except urllib.error.HTTPError:
-									print("404")
-							else :
+							# Ajout dans la structure de mapping de l'image uploadee
+							if id_media not in inverse_mapping:
+								id = len(inverse_mapping)
+								inverse_mapping[id_media] = id
+								mapping[id] = {"origin": origin, "type": "img_upload", "tweets": [id_tweet], "description":text_tweet}
 
-								content = req.content
-								soup = BeautifulSoup(content, 'html.parser')
 
-								if soup.find("title"):
+						except urllib.error.HTTPError:
+							print("404")
 
-									type_lien = getType(soup)
 
-									if type_lien == "video" :
-										# Une video ne pouvant pas être telechargée, on la décrit par son url et sa description
-										desc = extractDescription(soup)
-										print(desc)
-										# TODO Définir comment seront stockés les documents de type video
+				# On s'occupe des entités (urls + images)
+				if "urls" in tw["entities"] :
+					for u in tw["entities"]["urls"] :
+						url = u["expanded_url"]
+						#print("************************************")
+						print("\t",url)
+						try :
+							req = requests.get(url,verify=False, timeout=5)
+
+							if req.status_code == 200 :
+								id_media = req.url
+								origin = req.url
+
+								# Gestion image (le lien est directement une image) :
+								if isImg(req.url) :
+									try:
+										id_media = req.url
+										origin = req.url
+										urllib.request.urlretrieve(req.url, dir_img + "remote_" + req.url.split("/")[-1])
 
 										# Ajout dans la structure de mapping de l'image url
 										if id_media not in inverse_mapping:
 											id = len(inverse_mapping)
 											inverse_mapping[id_media] = id
-											mapping[id] = {"origin": origin, "type": "video", "tweets": [id_tweet], "description": text_tweet+" "+desc}
-										else :
-											mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
-											mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
-
-									elif type_lien == "image" :
-										origin = soup.find("meta", property="og:image")["content"]
-										id_media = origin.split("/")[-1]
-										urllib.request.urlretrieve(origin,dir_img + "html_" + id_media)
-										desc = extractDescription(soup)
-
-										# Ajout dans la structure de mapping de l'image "html"
-										if id_media not in inverse_mapping:
-											id = len(inverse_mapping)
-											inverse_mapping[id_media] = id
-											mapping[id] = {"origin": origin, "type": "img_html", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+											mapping[id] = {"origin": origin, "type": "img_url", "tweets": [id_tweet], "description": text_tweet}
 										else :
 											mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
 											mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
 
 
-									elif type_lien == "news" :
-										desc = extractDescription(soup)
-										# Ajout dans la structure de mapping de la news
-										if id_media not in inverse_mapping:
-											id = len(inverse_mapping)
-											inverse_mapping[id_media] = id
-											mapping[id] = {"origin": origin, "type": "news", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+
+									except urllib.error.HTTPError:
+										print("404")
+								else :
+
+									content = req.content
+									soup = BeautifulSoup(content, 'html.parser')
+
+									if soup.find("title"):
+
+										type_lien = getType(soup)
+
+										if type_lien == "video" :
+											# Une video ne pouvant pas être telechargée, on la décrit par son url et sa description
+											desc = extractDescription(soup)
+											print(desc)
+											# TODO Définir comment seront stockés les documents de type video
+
+											# Ajout dans la structure de mapping de l'image url
+											if id_media not in inverse_mapping:
+												id = len(inverse_mapping)
+												inverse_mapping[id_media] = id
+												mapping[id] = {"origin": origin, "type": "video", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+											else :
+												mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
+												mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+
+										elif type_lien == "image" :
+											origin = soup.find("meta", property="og:image")["content"]
+											id_media = origin.split("/")[-1]
+											urllib.request.urlretrieve(origin,dir_img + "html_" + id_media)
+											desc = extractDescription(soup)
+
+											# Ajout dans la structure de mapping de l'image "html"
+											if id_media not in inverse_mapping:
+												id = len(inverse_mapping)
+												inverse_mapping[id_media] = id
+												mapping[id] = {"origin": origin, "type": "img_html", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+											else :
+												mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
+												mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+
+
+										elif type_lien == "news" :
+											desc = extractDescription(soup)
+											# Ajout dans la structure de mapping de la news
+											if id_media not in inverse_mapping:
+												id = len(inverse_mapping)
+												inverse_mapping[id_media] = id
+												mapping[id] = {"origin": origin, "type": "news", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+											else :
+												mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
+												mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+
+
 										else :
-											mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
-											mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+											#print(req.content)
+											desc = extractDescription(soup)
+											# Ajout dans la structure de mapping de la news
+											if id_media not in inverse_mapping:
+												id = len(inverse_mapping)
+												inverse_mapping[id_media] = id
+												mapping[id] = {"origin": origin, "type": "website", "tweets": [id_tweet], "description": text_tweet+" "+desc}
+											else :
+												mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
+												mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
 
 
-									else :
-										#print(req.content)
-										desc = extractDescription(soup)
-										# Ajout dans la structure de mapping de la news
-										if id_media not in inverse_mapping:
-											id = len(inverse_mapping)
-											inverse_mapping[id_media] = id
-											mapping[id] = {"origin": origin, "type": "website", "tweets": [id_tweet], "description": text_tweet+" "+desc}
-										else :
-											mapping[inverse_mapping[id_media]]["tweets"].append(id_tweet)
-											mapping[inverse_mapping[id_media]]["description"] += (" "+text_tweet)
+										desc_lien = soup.find("meta",  property="og:description")
+										image_lien = soup.find("meta", property="og:image")
 
-
-									desc_lien = soup.find("meta",  property="og:description")
-									image_lien = soup.find("meta", property="og:image")
-
-								#print("AFFICHAGE DES META")
-								#print(type_lien)
-								#print(desc_lien)
-								#print(image_lien)
-								#sys.exit()
+									#print("AFFICHAGE DES META")
+									#print(type_lien)
+									#print(desc_lien)
+									#print(image_lien)
+									#sys.exit()
 
 
 
 
 
 
-						#print(req.content)
-					except requests.exceptions.Timeout :
-						print("socket.timeout")
-					except requests.exceptions.ConnectionError :
-						print("socket.gaierror")
+							#print(req.content)
+						except requests.exceptions.Timeout :
+							print("socket.timeout")
+						except requests.exceptions.ConnectionError :
+							print("socket.gaierror")
 
-	for id in mapping :
-		obj = mapping[id]
-		if "tweet" in obj["type"]:
-			with open(dir_index+"tweets.xml","a") as f :
-				f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
-		elif "img" in obj["type"]:
-			with open(dir_index+"images.xml","a") as f :
-				f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
-		elif "video" in obj["type"]:
-			with open(dir_index+"videos.xml","a") as f :
-				f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
-		elif "news" in obj["type"]:
-			with open(dir_index+"news.xml","a") as f :
-				f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
-		elif "website" in obj["type"]:
-			with open(dir_index+"websites.xml","a") as f :
-				f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
+		for id in mapping :
+			obj = mapping[id]
+			if "tweet" in obj["type"]:
+				with open(dir_index+"tweets.xml","a") as f :
+					f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
+			elif "img" in obj["type"]:
+				with open(dir_index+"images.xml","a") as f :
+					f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
+			elif "video" in obj["type"]:
+				with open(dir_index+"videos.xml","a") as f :
+					f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
+			elif "news" in obj["type"]:
+				with open(dir_index+"news.xml","a") as f :
+					f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
+			elif "website" in obj["type"]:
+				with open(dir_index+"websites.xml","a") as f :
+					f.write("<DOC>\n<DOCNO>"+str(id)+"</DOCNO>\n"+obj["description"]+"\n</DOC>\n")
 
-	json_file = dir_mapping+"mapping.json"
-	with open(json_file, 'w') as outfile:
-		json.dump(mapping, outfile)
+		json_file = dir_mapping+"mapping.json"
+		with open(json_file, 'w') as outfile:
+			json.dump(mapping, outfile)
 
-	json_file = dir_mapping+"inverse.json"
-	with open(json_file, 'w') as outfile:
-		json.dump(inverse_mapping, outfile)
+		json_file = dir_mapping+"inverse.json"
+		with open(json_file, 'w') as outfile:
+			json.dump(inverse_mapping, outfile)
 
 
 	#sys.exit()
