542ba7141c2af8f5fda7ced9e85b8d6bead99c63
yoPitarch <pitarch@irit.fr>
2017-03-09 16:34:25 +0100

    Add features.txt

--- featureCalculation/socialFeatures.py
+++ featureCalculation/socialFeatures.py
@@ -10,15 +10,22 @@ def lookupTweet(id) :
 
 path_events = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/"
 path_json =  "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Preliminaries/"
+path_features = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Features/"
+
 
 name_dir_mapping = "/mapping/mapping.json"
 name_dir_inverse = "/mapping/inverse.json"
 
+features = {}
 
 for repEvent in os.listdir(path_events) :
 	json_raw = {}
 	mapping = {}
 	inverse = {}
+
+	if not os.path.isfile(path_features+repEvent) :
+		os.mkdir(path_features+repEvent)
+
 	with open(path_json+"data_"+repEvent+".txt", "r") as fjson :
 		json_raw = json.load(fjson)
 
@@ -30,8 +37,22 @@ for repEvent in os.listdir(path_events) :
 
 	for idDoc in mapping :
 
+		favourites = 0
+		retweets = 0
+		followers = 0
+
+		features[idDoc]
+
 		for tw in mapping[idDoc]["tweets"] :
 			data = lookupTweet(int(tw))
 			print(data)
 
-			sys.exit()
+			favourites += data["favourite_count"]
+			retweets += data["retweet_count"]
+			followers += data["user"]["followers_count"]
+
+		features[idDoc]["favourite_count"] = favourites
+		features[idDoc]["retweet_count"] = retweets
+		features[idDoc]["followers_count"] = followers
+
+	json.dump(features,path_features+repEvent+"/social.json")
