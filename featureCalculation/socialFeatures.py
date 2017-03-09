779be15e9735cec556108c8f037998c4dadd9c8b
yoPitarch <pitarch@irit.fr>
2017-03-09 16:19:15 +0100

    Add features.txt

--- featureCalculation/socialFeatures.py
+++ featureCalculation/socialFeatures.py
@@ -1,6 +1,13 @@
 import os
 import json
 
+
+def lookupTweet(id) :
+	for j in json_raw :
+		if j["id"] == id :
+			return j
+
+
 path_events = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/"
 path_json =  "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Preliminaries/"
 
@@ -9,4 +16,20 @@ name_dir_inverse = "/mapping/inverse.json"
 
 
 for repEvent in os.listdir(path_events) :
-	with open(path_json+repEvent)
+	json_raw = {}
+	mapping = {}
+	inverse = {}
+	with open(path_json+"data_"+repEvent+".txt", "r") as fjson :
+		json_raw = json.load(fjson)
+
+	with open(path_events+repEvent+name_dir_mapping, "r") as fjson :
+		mapping = json.load(fjson)
+
+	with open(path_events+repEvent+name_dir_inverse, "r") as fjson :
+		inverse = json.load(fjson)
+
+	for idDoc in mapping :
+
+		for tw in mapping[idDoc]["tweets"] :
+			data = lookupTweet(int(tw))
+			print(data)
