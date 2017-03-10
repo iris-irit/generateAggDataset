2412d93ccf8d5cb1d4c0028bddfc4ad45ebe6948
yoPitarch <pitarch@irit.fr>
2017-03-10 09:03:56 +0100

    Add features.txt

--- featureCalculation/otherFunctions.py
+++ featureCalculation/otherFunctions.py
@@ -19,7 +19,7 @@ def getHistogram(json):
 		#pprint.pprint(doc)
 
 		d = datetime.strptime(doc["created_at"], '%a %b %d %H:%M:%S %z %Y')
-		print(d.strftime('%Y-%m-%d at %H'))
+		print(d.strftime('%Y-%m-%d at %H:%M'))
 
 		key = str(d.day)+"_"+str(d.hour)+"_"+str(d.minute)
 		hist.setdefault(key,0)
