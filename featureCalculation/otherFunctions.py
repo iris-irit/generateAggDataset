042a055b878ed21f11e1471e1b0648059d708b87
yoPitarch <pitarch@irit.fr>
2017-03-10 08:46:35 +0100

    Add features.txt

--- featureCalculation/otherFunctions.py
+++ featureCalculation/otherFunctions.py
@@ -1,6 +1,12 @@
 import pprint
+import sys
+from datetime import datetime
 
 def getDate(json):
 
 	for doc in json :
 		pprint.pprint(doc)
+
+		d = datetime.strptime(doc["created_at"], '%a %b %d %H:%M:%S %z %Y')
+		print(d.strftime('%Y-%m-%d'))
+		sys.exit()
