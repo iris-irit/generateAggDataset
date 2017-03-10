11c5b5882cd358f8c0d37e8ebdd9bb36ba0d8fb2
yoPitarch <pitarch@irit.fr>
2017-03-10 08:48:00 +0100

    Add features.txt

--- featureCalculation/otherFunctions.py
+++ featureCalculation/otherFunctions.py
@@ -8,5 +8,5 @@ def getDate(json):
 		pprint.pprint(doc)
 
 		d = datetime.strptime(doc["created_at"], '%a %b %d %H:%M:%S %z %Y')
-		print(d.strftime('%Y-%m-%d'))
+		print(d.strftime('%Y-%m-%d at %H'))
 		sys.exit()
