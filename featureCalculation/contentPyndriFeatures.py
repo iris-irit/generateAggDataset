6eafedd50aef04e2ee6cf81e7caea090ac06594a
iris@irit <yoann.pitarch@irit.fr>
2017-03-14 14:37:28 +0100

    ajout de contentPyndriFeatures.py

---
+++ featureCalculation/contentPyndriFeatures.py
@@ -0,0 +1,59 @@
+import os, sys
+import json
+
+
+def lookupTweet(id) :
+	for j in json_raw :
+		if j["id"] == id :
+			return j
+
+
+path_events = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Raw/"
+path_json =  "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Preliminaries/"
+path_features = "/projets/iris/PROJETS/PRINCESS/TournAgg/Datasets/Features/"
+
+
+name_dir_mapping = "/mapping/mapping.json"
+name_dir_inverse = "/mapping/inverse.json"
+
+features = {}
+
+for repEvent in os.listdir(path_events) :
+	json_raw = {}
+	mapping = {}
+	inverse = {}
+
+	if not os.path.exists(path_features+repEvent) :
+		os.mkdir(path_features+repEvent)
+
+	with open(path_json+"data_"+repEvent+".txt", "r") as fjson :
+		json_raw = json.load(fjson)
+
+	with open(path_events+repEvent+name_dir_mapping, "r") as fjson :
+		mapping = json.load(fjson)
+
+	with open(path_events+repEvent+name_dir_inverse, "r") as fjson :
+		inverse = json.load(fjson)
+
+	for idDoc in mapping :
+
+		favourites = 0
+		retweets = 0
+		followers = 0
+
+		features[idDoc] = {}
+
+		for tw in mapping[idDoc]["tweets"] :
+			data = lookupTweet(int(tw))
+			print(data)
+
+			favourites += data["favorite_count"]
+			retweets += data["retweet_count"]
+			followers += data["user"]["followers_count"]
+
+		features[idDoc]["favorite_count"] = favourites
+		features[idDoc]["retweet_count"] = retweets
+		features[idDoc]["followers_count"] = followers
+
+	with open(path_features+repEvent+"/social.json", "w") as fout :
+		json.dump(features,fout)
