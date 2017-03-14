04b4d27d7390ab0cd9cd69871f1779fd81d7347d
hubert <hubert@irit.fr>
2017-03-14 15:15:51 +0100

    modif

--- featureCalculation/contentFeatures.py
+++ featureCalculation/contentFeatures.py
@@ -46,7 +46,7 @@ for repEvent in os.listdir(path_events):
             print(data)
 
             urls += len(data["entities"]["urls"])
-            hashtags += len(data["entities"]["hahstags"])
+            hashtags += len(data["entities"]["hashtags"])
 
         features[idDoc]["url_count"] = urls
         #		features[idDoc]["url_exist"] = urls
