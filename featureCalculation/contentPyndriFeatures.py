b85ccb95a4fd21b820f4a7824a79c07c07ac2c8c
yoPitarch <pitarch@irit.fr>
2017-03-15 09:44:10 +0100

    Add features.txt

--- featureCalculation/contentPyndriFeatures.py
+++ featureCalculation/contentPyndriFeatures.py
@@ -27,92 +27,92 @@ events = os.listdir(path_events)
 
 tree = etree.parse(path_queries)
 for q in tree.xpath("/parameters/query"):
-    tobedone = "false"
-    for element in q.iter():
-        #print("%s - %s" % (element.tag, element.text))
-        # si la requete fait partie des requetes à tester, on doit lancer Indri
-        if (element.tag=="number") and (element.text in events): 
-        	tobedone="true"
-        	query = element.text
-        if (element.tag=="text") and tobedone=="true":
-            print("----------------"+element.text )
-            with pyndri.open(path_index+query) as index:
-                features = {}
-                num_documents=0
-                for document_id in range(index.document_base(), index.maximum_document()):
-                	num_documents += 1
-                	ext_document_id, _ = index.document(document_id)
-                	features[ext_document_id] = {}
-                	features[ext_document_id]["lm"] = 0
-                	features[ext_document_id]["tfidf"] = 0
-                	features[ext_document_id]["bm25"] = 0
+	tobedone = "false"
+	for element in q.iter():
+		#print("%s - %s" % (element.tag, element.text))
+		# si la requete fait partie des requetes à tester, on doit lancer Indri
+		if (element.tag=="number") and (element.text in events):
+			tobedone="true"
+			query = element.text
+		if (element.tag=="text") and tobedone=="true":
+			print("----------------"+element.text )
+			with pyndri.open(path_index+query) as index:
+				features = {}
+				num_documents=0
+				for document_id in range(index.document_base(), index.maximum_document()):
+					num_documents += 1
+					ext_document_id, _ = index.document(document_id)
+					features[ext_document_id] = {}
+					features[ext_document_id]["lm"] = 0
+					features[ext_document_id]["tfidf"] = 0
+					features[ext_document_id]["bm25"] = 0
 
 
-                # Constructs a QueryEnvironment that uses a
-                # language model with Dirichlet smoothing.	
-                lm_query_env = pyndri.QueryEnvironment(index, rules=('method:dirichlet,mu:5000',))
-                #results = lm_query_env.query(element.text, results_requested=num_documents, include_snippets=True)
-                results = lm_query_env.query(element.text, results_requested=num_documents)
-                minscore=0
-                maxscore = - np.inf
-                for int_document_id, score in results:
-                    ext_document_id, _ = index.document(int_document_id)
-                    if score< minscore:
-                    	minscore=score
-                    if score > maxscore:
-                    	maxscore = score
-                    #print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
-                    print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
-                    features[ext_document_id]["lm"] = score
-                    #print()
-                print(str(len(results))+" / "+str(num_documents))
-                
-                # Constructs a QueryEnvironment that uses the TF-IDF retrieval model.
-                #
-                # See "Baseline (non-LM) retrieval"
-                # (https://lemurproject.org/doxygen/lemur/html/IndriRunQuery.html)
-                tfidf_query_env = pyndri.TFIDFQueryEnvironment(index)
-                resultstfidf = tfidf_query_env.query(element.text, results_requested=num_documents)
-                maxscoretf = 0
-                for int_document_id, score in resultstfidf:
-                    ext_document_id, _ = index.document(int_document_id)
-                    if score > maxscoretf:
-                    	maxscoretf = score
-                    #print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
-                    print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
-                    features[ext_document_id]["tfidf"] = score
-                    #print()
-                print(str(len(results))+" / "+str(num_documents))
+				# Constructs a QueryEnvironment that uses a
+				# language model with Dirichlet smoothing.
+				lm_query_env = pyndri.QueryEnvironment(index, rules=('method:dirichlet,mu:5000',))
+				#results = lm_query_env.query(element.text, results_requested=num_documents, include_snippets=True)
+				results = lm_query_env.query(element.text, results_requested=num_documents)
+				minscore=0
+				maxscore = - np.inf
+				for int_document_id, score in results:
+					ext_document_id, _ = index.document(int_document_id)
+					if score< minscore:
+						minscore=score
+					if score > maxscore:
+						maxscore = score
+					#print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
+					print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
+					features[ext_document_id]["lm"] = score
+					#print()
+				print(str(len(results))+" / "+str(num_documents))
 
-                # Constructs a QueryEnvironment that uses the Okapi BM25 retrieval model.
-                #See "Baseline (non-LM) retrieval"
-                #(https://lemurproject.org/doxygen/lemur/html/IndriRunQuery.html)
-                bm25_query_env = pyndri.OkapiQueryEnvironment(index)
-                resultsbm = bm25_query_env.query(element.text, results_requested=num_documents)
-                maxscorebm25 = 0
-                for int_document_id, score in resultsbm:
-                    ext_document_id, _ = index.document(int_document_id)
-                    if score > maxscorebm25:
-                    	maxscorebm25 = score
-                    #print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
-                    print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
-                    features[ext_document_id]["bm25"] = score
-                    #print()
-                print(str(len(results))+" / "+str(num_documents))
+				# Constructs a QueryEnvironment that uses the TF-IDF retrieval model.
+				#
+				# See "Baseline (non-LM) retrieval"
+				# (https://lemurproject.org/doxygen/lemur/html/IndriRunQuery.html)
+				tfidf_query_env = pyndri.TFIDFQueryEnvironment(index)
+				resultstfidf = tfidf_query_env.query(element.text, results_requested=num_documents)
+				maxscoretf = 0
+				for int_document_id, score in resultstfidf:
+					ext_document_id, _ = index.document(int_document_id)
+					if score > maxscoretf:
+						maxscoretf = score
+					#print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
+					print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
+					features[ext_document_id]["tfidf"] = score
+					#print()
+				print(str(len(results))+" / "+str(num_documents))
+
+				# Constructs a QueryEnvironment that uses the Okapi BM25 retrieval model.
+				#See "Baseline (non-LM) retrieval"
+				#(https://lemurproject.org/doxygen/lemur/html/IndriRunQuery.html)
+				bm25_query_env = pyndri.OkapiQueryEnvironment(index)
+				resultsbm = bm25_query_env.query(element.text, results_requested=num_documents)
+				maxscorebm25 = 0
+				for int_document_id, score in resultsbm:
+					ext_document_id, _ = index.document(int_document_id)
+					if score > maxscorebm25:
+						maxscorebm25 = score
+					#print('Document {ext_document_id} retrieved with score {score}, snippet : {snippet}.'.format(ext_document_id=ext_document_id, score=score, snippet=snippet))
+					print('Document {ext_document_id} retrieved with score {score}.'.format(ext_document_id=ext_document_id, score=score))
+					features[ext_document_id]["bm25"] = score
+					#print()
+				print(str(len(results))+" / "+str(num_documents))
 
 
 
-                for doc in features:
-                	#print (doc + " - " +str(features[doc]["lm"]))
-                	if (features[doc]["lm"]!=0):
-                		features[doc]["lm"] = 1 - (features[doc]["lm"]/minscore) + maxscore/minscore
-                	if (features[doc]["tfidf"]>0):
-                		features[doc]["tfidf"] = features[doc]["tfidf"]/maxscoretf
-                	if (features[doc]["bm25"]>0):
-                		features[doc]["bm25"] = features[doc]["bm25"]/maxscorebm25
+				for doc in features:
+					#print (doc + " - " +str(features[doc]["lm"]))
+					if (features[doc]["lm"]!=0):
+						features[doc]["lm"] = 1 - (features[doc]["lm"]/minscore) + maxscore/minscore
+					if (features[doc]["tfidf"]>0):
+						features[doc]["tfidf"] = features[doc]["tfidf"]/maxscoretf
+					if (features[doc]["bm25"]>0):
+						features[doc]["bm25"] = features[doc]["bm25"]/maxscorebm25
 
-                with open(path_features+query+"/indri.json", "w") as fout :
-                	json.dump(features,fout)
+				with open(path_features+query+"/indri.json", "w") as fout :
+					json.dump(features,fout)
 
 		# Constructs a QueryEnvironment that uses the TF-IDF retrieval model.
 		#
@@ -130,7 +130,7 @@ for q in tree.xpath("/parameters/query"):
 
 
 
-        	
+
 
 # for repEvent in os.listdir(path_events) :
 # 	json_raw = {}
