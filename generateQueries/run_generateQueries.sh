67718bae4c6e3676d2ade4bd60e4b76f38032041
yoPitarch <pitarch@irit.fr>
2017-03-21 15:13:42 +0100

    Add features.txt

--- generateQueries/run_generateQueries.sh
+++ generateQueries/run_generateQueries.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 #SBATCH --job-name=getRawData
+#SBATCH --error=/projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/generateQueries/error.txt
 #SBATCH --mail-user=pitarch@irit.fr
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
 
-srun /logiciels/Python-3.5.2/bin/python3 /projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateQueries/generateQuery.py
+srun /logiciels/Python-3.5.2/bin/python3 /projets/iris/PROJETS/PRINCESS/TournAgg/Code/generateAggDataset/generateQueries/generateQuery.py
